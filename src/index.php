3507803eb25ec724be35ea7ea88c0ce305738d88
Таня <taaania@bk.ru>
2015-01-23 17:02:32 +0200

    Для Вови

--- src/index.php
+++ src/index.php
@@ -1,28 +1,28 @@
 <?php
 
-require_once __DIR__ . '/core/Rozklad.php';
+require_once __DIR__ . '/core/Bootstrap.php';
 require_once __DIR__ . '/core/Controller.php';
 require_once __DIR__ . '/core/Model.php';
 require_once __DIR__ . '/core/View.php';
 require_once __DIR__ . '/core/database.php';
 require_once __DIR__ . '/models/role_model.php';
 
-$app = new Rozklad();
+$app = new Bootstrap();
 
-/*$select = array(
+$select = array(
     'where' => "title = 'student'"
 );
-$model = new Model_role($select);
-$model->title ='student';
-$model->save();
-print_r( $model -> getAllRows());
+//$model = new Model_role($select);
+//$model->title ='student';
+//$model->save();
+//print_r( $model -> getAllRows());
 
 // приклад роботи з базою
-try {
-$db = DataBase::getInstance();
-    print_r($db->query('SELECT * from role')->fetchAll(PDO::FETCH_ASSOC));
-} catch(PDOException $e) {
-    print 'error';
-}*/
+//try {
+//$db = DataBase::getInstance();
+//    print_r($db->query('SELECT * from role')->fetchAll(PDO::FETCH_ASSOC));
+//} catch(PDOException $e) {
+//    print 'error';
+//}
 
 ?>
