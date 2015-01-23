d78a6c6afa96cca5749b94089ec3894d7f205e12
konstanchuk <vova.konstanchuk@gmail.com>
2015-01-23 17:19:13 +0200

    +

--- src/core/Request.php
+++ src/core/Request.php
@@ -1,11 +1,60 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Таня
- * Date: 22.01.2015
- * Time: 23:40
- */
-class Request{
 
+class Request {
+    private static $instance = null;
+
+    private $data = array();
+    private $controller = null;
+    private $action = null;
+
+    function __construct() {
+        $this->parseGet();
+    }
+
+    private function parseGet() {
+        $url = explode('/', rtrim($_GET['url'], '/'));
+        if (isset($url[0])) {
+            $this->controller = $url[0];
+            if (isset($url[1])) {
+                $this->action = $url[1];
+                for($i = 2; $i<=count($url)-1; $i+=2) {
+                    if (isset($url[$i+1])) {
+                        $this->data[$url[$i]] = $url[$i+1];
+                    } else {
+                        $this->data[$url[$i]] = null;
+                    }
+                }
+            }
+        } else {
+            $this->controller = 'index';
+        }
+    }
+
+    function getParam($name){
+        return $this->data[$name];
+    }
+
+    function getAction(){
+        return $this->action;
+    }
+
+    function getController(){
+        return $this->controller;
+    }
+
+    function getParams() {
+        return $this->data;
+    }
+
+    private function  __clone(){
+
+    }
+
+    public static function getInstance(){
+        if(!self::$instance){
+            self::$instance = new Request();
+        }
+        return self::$instance;
+    }
 }
 ?>
