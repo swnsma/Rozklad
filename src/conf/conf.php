1a6ef1bcb4f56d31c04c991111ec6741d55a2af6
swnsma <swnsma@gmail.com>
2015-01-23 16:07:58 +0200

    1)DB dump rewritten.

--- src/conf/conf.php
+++ src/conf/conf.php
@@ -5,40 +5,46 @@ function baseConfig(){
     $buff2="";
     $path='../SQL/install';
     $query="";
-    $file="";
-    $files=scandir($path);
     if(file_exists($path."/version.txt")){
         $file=fopen($path."/version.txt", 'r+');
         $buff=fgets($file, 10);
         fclose($file);
     }
-    if(($buff2=preg_grep('/install_[0-9]*\.sql/',$files))){
-        //ну что поделать, говнокодим.
-        foreach($buff2 as $file_name);
-        $buff2=preg_replace("/[^0-9]/", '',$file_name);
-    }
-    if($buff==$buff2){
-        $file=fopen($path."/version.txt", 'w+');
-        fputs($file, $buff2);
+    if(file_exists($path."/last_version.txt")){
+        $file=fopen($path."/last_version.txt", 'r+');
+        $buff2=fgets($file, 10);
         fclose($file);
-        $file=fopen($path.'/install_'.$buff2.'.sql', 'r');
+    }
+    if($buff!=$buff2){
         try{
-        $DBH=new PDO("sqlite: ../SQL/data/rozklad.sqlite");
-            while($buff=fgets($file, 1000)){
-                if(preg_match('/DROP/', $buff)){
-                    $DBH->query($buff);
-                    if(count_chars($query)){
-                        $DBH->query($query);
-                        $query="";
+            $buff=0;
+            //переделать
+            $DBH=new PDO("sqlite:C:/Git/Rozklad/src/SQL/data/rozklad.sqlite");
+            do{
+                $buff++;
+                $file=fopen($path.'/install_'.$buff2.'.sql', 'r');
+                while($buff_query=fgets($file, 1000)){
+                    if(preg_match('/DROP/', $buff_query)){
+                        $DBH->query($buff_query);
+                        if(count_chars($query)){
+                            $DBH->query($query);
+                            $query="";
+                        }
+                    }else{
+                        $query=$query.$buff_query;
                     }
-                }else{
-                    $query=$query.$buff;
                 }
-            }
+                fclose($file);
+            }while($buff!=$buff2);
+            $file=fopen($path."version.txt", "w");
+            fputs($file, $buff2);
+            fclose($file);
             $DBH=null;
         }
         catch(PDOException $e) {
             print 'Error';
+            $DBH=null;
         }
     }
 }
+baseConfig();
