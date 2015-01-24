ae3e3210fc2b650e5ab092aab4d4a2c1564f8779
konstanchuk <vova.konstanchuk@gmail.com>
2015-01-24 17:15:35 +0200

    +

--- src/core/Bootstrap.php
+++ src/core/Bootstrap.php
@@ -4,12 +4,13 @@ class Bootstrap {
     function __construct() {
         $request = Request::getInstance();
         $controller = $request->getController();
-        $file = __DIR__ . '/../controllers/' . $controller . '.php';
+        $module = $request->getModule();
+        $file = __DIR__ . '/../controllers/' . $module . '/' . $controller . '.php';
         if (file_exists($file)) {
             require $file;
             $c = new $controller;
         } else {
-            require __DIR__ . '/../controllers/error.php';
+            require __DIR__ . '/../controllers/app/error.php';
             $c = new Error();
         }
         $c->run($request->getAction());
--- src/core/Request.php
+++ src/core/Request.php
@@ -4,8 +4,10 @@ class Request {
     private static $instance = null;
 
     private $data = array();
-    private $controller = null;
-    private $action = null;
+    private $controller = 'index',
+        $action = 'index',
+        $module = 'app';
+
 
     function __construct() {
         $this->parseGet();
@@ -14,22 +16,21 @@ class Request {
     private function parseGet() {
         if (isset($_GET['url'])) {
             $url = explode('/', rtrim($_GET['url'], '/'));
-            $this->controller = $url[0];
+            $this->module = $url[0];
             if (isset($url[1])) {
-                $this->action = $url[1];
-                for($i = 2; $i<=count($url)-1; $i+=2) {
-                    if (isset($url[$i+1])) {
-                        $this->data[$url[$i]] = $url[$i+1];
-                    } else {
-                        $this->data[$url[$i]] = null;
+                $this->controller = $url[1];
+                if (isset($url[2])) {
+                    $this->action = $url[2];
+                    $c = count($url);
+                    for($i = 3; $i<$c; $i+=2) {
+                        if (isset($url[$i+1])) {
+                            $this->data[$url[$i]] = $url[$i+1];
+                        } else {
+                            $this->data[$url[$i]] = null;
+                        }
                     }
                 }
-            } else {
-                $this->action = 'index';
             }
-        } else {
-            $this->controller = 'index';
-            $this->action = 'index';
         }
     }
 
@@ -39,6 +40,10 @@ class Request {
 
     function getAction(){
         return $this->action;
+    }
+
+    function getModule(){
+        return $this->module;
     }
 
     function getController(){
