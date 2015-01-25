f782a0041255c3b8ead188b66d809b03f6525240
Borys <mbl171495@gmail.com>
2015-01-25 14:33:29 +0200

    host

--- src/conf/conf.php
+++ src/conf/conf.php
@@ -1,7 +1,7 @@
 <?php
 
 //define('URL', 'http://rozklad.loc:83/src/');
-define('URL', 'http://rozklad:10000/src/');
+define('URL','http://'.$_SERVER['HTTP_HOST'].'/src/');
 define('DOCUMENT_ROOT', __DIR__ . '/../');
 
 ?>
