0dfcbdcb447f33c5a840a7cbbd53461f7ebb9cf8
swnsma <swnsma@gmail.com>
2015-01-26 18:13:42 +0200

    1) Занесено version.txt в .gitignore 2) Змінений дамп бд. Змінені поля на NULL по замовчуванню, змінена структура таблиці user 3) Переписаний дамп( працює з одним файлом) 4) Видалено conf_old.php 5) Створено BaseInstall.php в папці core

--- src/core/BaseInstall.php
+++ src/core/BaseInstall.php
@@ -1,24 +1,30 @@
 <?php
-require_once __DIR__ . '/../core/database.php';
-class BaseInstall{
+require_once  'database.php';
+class Base_Install{
     private function  __construct(){
     }
     public static function Run(){
         $buff="0";
-        $buff2="";
         $path='../SQL/install';
+        $version=0;
         $query="";
         if(file_exists($path."/version.txt")){
             $file=fopen($path."/version.txt", 'r+');
             $buff=fgets($file, 10);
             fclose($file);
         }
-        if(file_exists($path."/last_version.txt")){
-            $file=fopen($path."/last_version.txt", 'r+');
-            $buff2=fgets($file, 10);
-            fclose($file);
+        $dir = opendir($path);
+        while( $files=readdir($dir))
+        {
+            if(preg_match("/install_[0-9]*\.sql/", $files)){
+                $buff2=preg_replace("/[^0-9]/", "", $files);
+                if($version<$buff2){
+                    $version=$buff2;
+                }
+            }
         }
-        if($buff==$buff2){
+        $buff2=$version;
+        if($buff!=$buff2){
             try{
                 $buff=0;
                 //переделать
@@ -27,7 +33,7 @@ class BaseInstall{
                     $buff++;
                     $file=fopen($path.'/install_'.$buff2.'.sql', 'r');
                     while($buff_query=fgets($file, 1000)){
-                        if(preg_match('/DROP/', $buff_query)){
+                        if(preg_match('/DROP/', $buff_query)||preg_match('/INSERT/', $buff_query)){
                             $DBH->query($buff_query);
                             if(count_chars($query)){
                                 $DBH->query($query);
@@ -37,9 +43,12 @@ class BaseInstall{
                             $query=$query.$buff_query;
                         }
                     }
+                    if($query){
+                        $DBH->query($query);
+                    }
                     fclose($file);
                 }while($buff!=$buff2);
-                $file=fopen($path."version.txt", "w");
+                $file=fopen($path."/version.txt", "w");
                 fputs($file, $buff2);
                 fclose($file);
             }
