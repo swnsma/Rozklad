c9f61f44188f4d4799d89f167245721736b54143
swnsma <swnsma@gmail.com>
2015-01-27 15:58:09 +0200

    1)CREATE TABLE IF NOT EXISTS 2) Remove DROP TABLE

--- src/SQL/install/install_1.sql
+++ src/SQL/install/install_1.sql
@@ -1,22 +1,18 @@
-DROP TABLE IF EXISTS "group";
-CREATE TABLE "group" (
+CREATE TABLE IF NOT EXISTS "group" (
   "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
   "name" text NULL,
   "teacher_id" integer NULL,
   FOREIGN KEY ("id") REFERENCES "student-group" ("group_id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
-
-DROP TABLE IF EXISTS "group_lesson";
-CREATE TABLE "group_lesson" (
+CREATE TABLE IF NOT EXISTS"group_lesson" (
   "group_id" integer NOT NULL,
   "lesson_id" integer NOT NULL,
   PRIMARY KEY ("group_id", "lesson_id")
 );
 
 
-DROP TABLE IF EXISTS "lesson";
-CREATE TABLE "lesson" (
+CREATE TABLE IF NOT EXISTS"lesson" (
   "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
   "date" text NULL,
   "title" text NULL,
@@ -27,24 +23,21 @@ CREATE TABLE "lesson" (
 );
 
 
-DROP TABLE IF EXISTS "role";
-CREATE TABLE "role" (
+CREATE TABLE IF NOT EXISTS "role" (
   "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
   "title" integer NOT NULL,
   FOREIGN KEY ("id") REFERENCES "user" ("role_id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 
-DROP TABLE IF EXISTS "student_group";
-CREATE TABLE "student_group" (
+CREATE TABLE IF NOT EXISTS "student_group" (
   "student_id" integer NOT NULL,
   "group_id" integer NOT NULL,
   PRIMARY KEY ("student_id", "group_id")
 );
 
 
-DROP TABLE IF EXISTS "user";
-CREATE TABLE "user" (
+CREATE TABLE IF NOT EXISTS "user" (
   "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
   "name" text NULL,
   "surname" text NULL,
