cc426a67aeb04cf15c1a82e92c5cd1b41daf1fa5
Borys <mbl171495@gmail.com>
2015-01-27 16:59:00 +0200

    my login file

--- src/public/js/app/login1.js
+++ src/public/js/app/login1.js
@@ -1,1 +1,64 @@
 
+var auth_fb = (function() {
+
+    function statusChangeCallback(response) {
+        console.log('statusChangeCallback');
+        console.log(response);
+        if (response.status === 'connected') {
+            auth.check(response.authResponse.accessToken, 'facebook',
+                function() {
+                    alert('зараз відбудеття реєстрація');
+                    auth.register(user_data)
+                }
+            );
+        } else if (response.status === 'not_authorized') {
+            document.getElementById('status').innerHTML = 'Please log ' +
+            'into this app.';
+        } else {
+            document.getElementById('status').innerHTML = 'Please log ' +
+            'into Facebook.';
+        }
+    }
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '386967351480465',
+            cookie     : true,
+            xfbml      : true,
+            version    : 'v2.1'
+        });
+
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    function testAPI() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+            console.log(response);
+            console.log('Successful login for: ' + response.name);
+            document.getElementById('status').innerHTML =
+                'Thanks for logging in, ' + response.name + '!';
+        });
+    }
+
+    return {
+        checkLoginState: function() {
+            $('#fbLogin').click(function() {
+                FB.getLoginStatus(function(response) {
+                    statusChangeCallback(response);
+                });
+            });
+        }
+    }
+})().checkLoginState();
