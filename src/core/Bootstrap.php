dd61a5748a917e536c4ec653815cb0c63f3ed7d0
swnsma <swnsma@gmail.com>
2015-01-27 17:14:28 +0200

    1)ADDED autorun SQL install

--- src/core/Bootstrap.php
+++ src/core/Bootstrap.php
@@ -35,6 +35,7 @@ class Bootstrap {
         $controller = $request->getController();
         $module = $request->getModule();
         $file = FILE . 'module/' . $module . '/controllers/' . $controller . '.php';
+        Base_Install::Run();
         if (file_exists($file)) {
             require_once $file;
             $c = new $controller;
