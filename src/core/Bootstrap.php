5a4b2699d742f2bbab846b4755cc31e09132ca2a
Borys <mbl171495@gmail.com>
2015-01-27 17:14:20 +0200

    change root path

--- src/core/Bootstrap.php
+++ src/core/Bootstrap.php
@@ -1,33 +1,4 @@
 <?php
-require_once( URL.'facebook/Facebook/HttpClients/Httpable.php' );
-require_once( URL.'facebook/FacebookCurl.php' );
-require_once(URL.'/facebook/FacebookCurlHttpClient.php' );
-// added in v4.0.0
-require_once(URL.'/facebook/FacebookSession.php' );
-require_once( URL.'facebook/FacebookRedirectLoginHelper.php' );
-require_once( URL.'facebook/FacebookRequest.php' );
-require_once(URL.'/src/facebook/FacebookResponse.php' );
-require_once(URL.'/src/facebook/FacebookSDKException.php' );
-require_once( URL.'/facebook/FacebookRequestException.php' );
-require_once( URL.'/src/facebook/FacebookOtherException.php' );
-require_once(URL.'/src/facebook/FacebookAuthorizationException.php' );
-require_once(URL.'facebook/GraphObject.php' );
-require_once( URL.'facebook/GraphSessionInfo.php' );
-// added in v4.0.5
-use Facebook\FacebookHttpable;
-use Facebook\FacebookCurl;
-use Facebook\FacebookCurlHttpClient;
-// added in v4.0.0
-use Facebook\FacebookSession;
-use Facebook\FacebookRedirectLoginHelper;
-use Facebook\FacebookRequest;
-use Facebook\FacebookResponse;
-use Facebook\FacebookSDKException;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookOtherException;
-use Facebook\FacebookAuthorizationException;
-use Facebook\GraphObject;
-use Facebook\GraphSessionInfo;
 
 class Bootstrap {
     function __construct() {
