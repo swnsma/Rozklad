6fb062d053b3338176bb5d1c3039cbf8f50a6bf9
kovalenko0 <koolse@outlook.com>
2015-01-28 16:16:00 +0200

    add header as separate .phtml file

---
+++ src/public/js/app/common/header/header.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Sasha on 28.01.2015.
+ */
+function load(){
+    function getCurrentUser(){
+        $.ajax({
+            url: 'admin/getUnconfirmedUsers',//???????
+            success: function(response){
+                var photo = document.getElementById('activeUserPhoto');
+                debugger;
+            },
+            error: function(er) {
+                debugger;
+                console.dir(er);
+            }
+
+        });
+    }
+}
+
+function logout(){
+
+}
