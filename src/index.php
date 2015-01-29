a6aa54f9ee104232ed99306fd9ef25606d8b6433
swnsma <swnsma@gmail.com>
2015-01-29 16:20:25 +0200

    1) Щоб не було проблем з встановкою бази при відсутності version.txt та вже наявності готової бази, база буде дропатися і таблички встановлювати заново.

--- src/index.php
+++ src/index.php
@@ -9,7 +9,6 @@ require_once 'core/View.php';
 require_once 'core/BaseInstall.php';
 require_once 'core/Request.php';
 require_once 'core/Session.php';
-//Base_Install::DesolationBase();
 Base_Install::Run();
 //Base_Install::LoadDummy();
 $app = new Bootstrap();
