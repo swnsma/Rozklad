3fcc7db07fee1d8f0f25790a6ca5ac3a8c1181bf
Borys <mbl171495@gmail.com>
2015-01-29 11:39:09 +0200

    зміни в login

--- src/module/app/controllers/login.php
+++ src/module/app/controllers/login.php
@@ -1,15 +1,14 @@
 <?php
 
 class Login extends Controller {
+    public static $status;
     public function __construct() {
         parent::__construct();
-        echo "hi";
         $this->model = $this->loadModel('login');
     }
 
     public function index() {
-        $data = 'hi'; //викликаємо портрібні функції поделі
-        $this->view->renderHtml('signin/index', $data);
+        $this->view->renderHtml('signin/index');
     }
 }
 
