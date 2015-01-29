f631d66efae8b426efb0aa89a6d86bfc0740cf61
Borys <mbl171495@gmail.com>
2015-01-29 08:16:42 +0200

    модель перевірки

---
+++ src/module/app/model/check_model.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 27.01.2015
+ * Time: 22:34
+ */
+class CheckModel extends Model{
+    public function __construct() {
+        parent::__construct();
+    }
+    public function checkUserFB($id){
+        $user=$this->db->query("SELECT fb_id FROM user where fb_id=$id")->fetchAll(PDO::FETCH_ASSOC);
+        return count($user);
+    }
+    public function checkUserGM($id){
+        $user=$this->db->query("SELECT gm_id FROM user where gm_id=$id")->fetchAll(PDO::FETCH_ASSOC);
+        return count($user)>0;
+    }
+}
