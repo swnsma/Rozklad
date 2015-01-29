694433ea9b6d8a0c175aa609d483fcec3af3b727
Borys <mbl171495@gmail.com>
2015-01-29 11:42:39 +0200

    зміни в login1.js

--- src/public/js/app/signin/login1.js
+++ src/public/js/app/signin/login1.js
@@ -1,22 +1,11 @@
 
 var auth_fb = (function() {
-    //var login_event = function(response) {
-    //    console.log("login_event");
-    //    console.log(response.status);
-    //    console.log(response);
-    //}
-    //
-    //var logout_event = function(response) {
-    //    console.log("logout_event");
-    //    console.log(response.status);
-    //    console.log(response);
-    //}
     function statusChangeCallback(response) {
         console.log('statusChangeCallback');
         console.log(response);
         if (response.status === 'connected') {
             $.ajax({
-                url: url + 'app/check/',
+                url: url + 'app/check/check',
                 type: 'GET',
                 success: function (response) {
                     console.log(response);
@@ -32,45 +21,15 @@ var auth_fb = (function() {
                 }
             });
         }
-        // else if (response.status === 'not_authorized') {
-        //    if(window.location!==url+"app/login")
-        //    window.location=url+"app/login";
-        //} else {
-        //    var path = window.location;
-        //    if(path+""!==url+"app/login"){
-        //        window.location=url+"app/login";
-        //    }
-        //
-        //}
-        //$.ajax({
-        //    url: url + 'app/check/',
-        //    type: 'GET',
-        //    success: function(response) {
-        //        console.log(response);
-        //    },
-        //    error: function(error) {
-        //        console.log(error);
-        //        alert('error: block get status');
-        //    }
-        //});
     }
 
     window.fbAsyncInit = function() {
         FB.init({
-            appId      : '330194637170000',
+            appId      : '1536442079974268',
             cookie     : true,
             xfbml      : true,
             version    : 'v2.2'
         });
-        //FB.getLoginStatus(function(response) {
-        //    statusChangeCallback(response);
-        //});
-        //FB.Event.subscribe('auth.login', login_event);
-        //FB.Event.subscribe('auth.logout', logout_event);
-
-// In your JavaScript code:
-
-
     };
 
     (function(d, s, id) {
@@ -98,6 +57,7 @@ var auth_fb = (function() {
                     console.log("logIN RESP: "+resp.authResponse)
                     FB.getLoginStatus(function(response) {
                         statusChangeCallback(response);
+                        window.location.reload();
                     });
                 });
             });
@@ -128,28 +88,4 @@ var auth_fb = (function() {
         }
     }
 })().checkLoginState();
-$(document).ready(function(){
-    check_user("regist","signIn");
-    $(".form").toggle();
-})
 
-function check_user(sectionFrom,sectionSignIn){
-    $.ajax({
-        url: url + 'app/check/',
-        type: 'GET',
-        success: function (response) {
-            console.log(response);
-            if (response==="regist") {
-                $("#"+sectionFrom).toggle();
-                $("#"+sectionSignIn).toggle();
-            }
-            if (response==="ok") {
-                window.location=url+"app/calendar";
-            }
-        },
-        error: function (error) {
-            console.log(error);
-            alert('error: block get status');
-        }
-    });
-}
