c36f57c5ac494119d48145f57b75a361ddc261ab
konstanchuk <vova.konstanchuk@gmail.com>
2015-01-30 17:14:00 +0200

    сторінка списку груп

---
+++ src/module/app/controllers/creategroup.php
@@ -0,0 +1,12 @@
+<?php
+
+class CreateGroup extends Controller {
+    public function __construct() {
+        parent::__construct();
+
+    }
+
+    public function index() {
+        print 1234;
+    }
+}
