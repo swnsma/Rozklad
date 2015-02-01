b666c8f336270acd5320df860ad224ae12d8910f
Borys <mbl171495@gmail.com>
2015-02-01 22:30:50 +0200

    signin

---
+++ src/module/app/controllers/signin.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once FILE.'conf/setup.php';
+
+
+class Signin extends Controller {
+    public static $status="not";
+    public function __construct() {
+        parent::__construct();
+        unset($_COOKIE);
+    }
+    public function index(){
+//        print_r ($_COOKIE);
+        $this->view->renderHtml("signin/index","not");
+    }
+}
+
+?>
