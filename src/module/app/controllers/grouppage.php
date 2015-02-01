4d2df7ea595aadee33ad47ca8c521888bb0b9e51
swnsma <swnsma@gmail.com>
2015-02-01 17:41:08 +0200

    1) Grouppage role = host 2) DATABASE  Role id teacher = 1, id student = 0

--- src/module/app/controllers/grouppage.php
+++ src/module/app/controllers/grouppage.php
@@ -6,7 +6,7 @@
  * Time: 3:53 PM
  */
 class GroupPage extends Controller {
-    public static $role='student';
+    public $role='host';
     public function __construct() {
         parent::__construct();
         $this->model = $this->loadModel('grouppage');
@@ -14,8 +14,8 @@ class GroupPage extends Controller {
     }
 
     public function index() {
-        $data = 'hi'; //викликаємо портрібні функції поделі
-        $this->model->setgID(1);
+        $data['role'] = $this->$role ; //викликаємо портрібні функції поделі
+
         $this->view->renderHtml('grouppage/index', $data);
     }
     public function delUser(){
