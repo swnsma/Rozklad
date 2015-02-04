09f467ef9017c167fce6628590cd595053e8eb7a
konstanchuk <vova.konstanchuk@gmail.com>
2015-02-04 13:25:47 +0200

    database -> Database

--- src/index.php
+++ src/index.php
@@ -1,15 +1,7 @@
 <?php
 
-if (DEBUG) {
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-} else {
-    error_reporting(0);
-    ini_set('display_errors', 0);
-}
-
 require_once 'conf/conf.php';
-require_once 'core/DataBase.php';
+require_once 'core/Database.php';
 require_once 'core/Bootstrap.php';
 require_once 'core/Controller.php';
 require_once 'core/Model.php';
@@ -17,6 +9,15 @@ require_once 'core/View.php';
 require_once 'core/BaseInstall.php';
 require_once 'core/Request.php';
 require_once 'core/Session.php';
+
+
+if (DEBUG) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+} else {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+}
 
 Base_Install::Run();
 //Base_Install::LoadDummy();
