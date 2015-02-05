93ba2c6803775af2fe90089fc8f158b348e7bf3c
kovalenko0 <koolse@outlook.com>
2015-02-05 10:42:37 +0200

    configure constant paths

--- src/conf/conf.php
+++ src/conf/conf.php
@@ -1,6 +1,6 @@
 <?php
 
-define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '/src/');
+define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
 define('FILE', __DIR__ . '/../');
 define('IMAGES_FOLDER', FILE . 'cdn/images');
 
