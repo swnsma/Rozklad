5533d50ed6700a441d4a748162dd0c36da09d3d0
konstanchuk <vova.konstanchuk@gmail.com>
2015-02-06 00:42:08 +0200

    збереження зображень, вибір кольору групи

--- src/module/app/controllers/groups.php
+++ src/module/app/controllers/groups.php
@@ -53,6 +53,7 @@ class Groups extends Controller {
                             ));
                             return;
                         }
+
                     }
                     //$data = $this->model->createGroup($this->user_info['id'], $name, $descr, $image);
                     $data = $this->model->createGroup(1, $name, $descr, $image);
