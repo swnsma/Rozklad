2adcfb1ae8d94786e560c1c1582a340a007955e7
Таня <taaania@bk.ru>
2015-02-10 23:53:38 +0200

    calendar: виправлене щось

--- src/public/js/app/calendar/teacher.js
+++ src/public/js/app/calendar/teacher.js
@@ -134,12 +134,12 @@ function Calendar_teacher(){
         if((y+(+heightPopup)-yminus)>=height){
             y=y-heightPopup;
         }
-        if((x-(+widthPopup))/2<0){
-            x=1;
-        }
-        if((x+widthPopup)>width){
-            x=width-widthPopup;
-        }
+        //if((x-(+widthPopup)/2)<=0){
+        //    x=1;
+        //}else
+        //if((x+widthPopup/2)>=width){
+        //    x=width-widthPopup;
+        //}
         self.jqueryObject.popup.popup.css({
             'left':x,
             'top':y
