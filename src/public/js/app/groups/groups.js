7e4e85cd77c3122f206ae84e6a8950b6b579d95d
swnsma <swnsma@gmail.com>
2015-02-13 12:20:00 +0200

    1) Редагування. Прогрес пішов

--- src/public/js/app/groups/groups.js
+++ src/public/js/app/groups/groups.js
@@ -2,14 +2,22 @@ function ViewModel(){
     var that = this;
     that.groups = ko.observableArray([]);
     that.currentId="";
-    that.startEdit=function(name){
-        for(var i=0; i<that.groups().length; i++){
-            if (that.groups()[i].name==name){
-                that.groups()[i].edit(true);
-            }
-        }
-
-    };
+    that.archivate=function(obj){
+                $.ajax({
+                       url: url+'app/groups/moveToArchive/'+obj.groupId+'/'+1,
+                        type: 'GET',
+                        success: function(){
+                           for(var i =0;i<that.groups().length;i++){
+                                  if(that.groups()[i].groupId==obj.groupId){
+                                          that.groups()[i].archived(true);
+                                      }
+                               }
+                        },
+                    error: function (xhr){
+                            alert("Error! "+xhr);
+                        }
+                });
+        };
     that.activate = function(){
         universalAPI(url+'app/groups/getGroupList', 'GET', function(response){
             that.currentId= response[response.length-1];
@@ -19,7 +27,17 @@ function ViewModel(){
                 group.name = ko.observable(response[i].name);
                 group.description = ko.observable(response[i].description);
                 group.archived = ko.observable(response[i].archived==1);
-                group.edit = ko.observable();
+                group.edit = ko.observable(false);
+                group.startEditing = function(){
+                    this.edit(true);
+                };
+                group.cancelEditing = function(){
+                    this.edit(false);
+                };
+                group.sendChanges = function(){
+                    universalAPI(url+'app/grouppage/renameGroup/'+this.groupId,"POST", function(){}, function(){}, {title:this.name()});
+                    universalAPI(url+'app/grouppage/editDescription/'+this.groupId, "POST", function(){}, function(){}, {data:this.description()});
+                };
                 that.groups.push(group);
             }
             that.groups.sort(function(left, right) { return left.name == right.name ? 0 : (left.name < right.name ? -1 : 1) });
@@ -27,11 +45,10 @@ function ViewModel(){
     }
 }
 function Group(obj){
-    //this.name = obj.name;
-    //this.description=obj.descr;
     this.teacher = obj.teacher_fn+' '+obj.teacher_ln;
     this.imgSrc=url+'public/users_files/images/groups_photo/'+obj.photo;
     this.groupLink=url+'app/grouppage/id'+obj.group_id;
+    this.groupId=obj.group_id;
     this.goAway = function(){
         window.location = this.groupLink;
     }
