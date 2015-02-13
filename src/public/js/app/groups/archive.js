7cefbb41b6c23a1f34817b601cf28ce8b6f522e7
AndreyUser <vfreiby@gmail.com>
2015-02-13 02:36:37 +0200

    Віднолення групи з архіву

--- src/public/js/app/groups/archive.js
+++ src/public/js/app/groups/archive.js
@@ -3,6 +3,20 @@ function ViewModel(){
     that.groups = ko.observableArray([]);
     that.currentId="";
 
+
+
+    that.restore=function(groupId){
+        $.ajax({
+            url: url+'app/groups/moveToArchive/'+groupId+'/'+0,
+            type: 'GET',
+            success: function(){
+               location.href=url+'app/groups';
+            },
+            error: function (xhr){
+                alert("Error! "+xhr);
+            }
+        });
+    };
     that.activate = function(){
         universalAPI(url+'app/groups/getArchiveList', 'GET', function(response){
             that.currentId= response[response.length-1];
@@ -11,6 +25,7 @@ function ViewModel(){
                 group.name = ko.observable(response[i].name);
                 that.groups.push(group);
             }
+            console.log(response)
             that.groups.sort(function(left, right) { return left.name == right.name ? 0 : (left.name < right.name ? -1 : 1) });
         })
     }
@@ -19,7 +34,25 @@ function ViewModel(){
 function Group(obj){
     this.teacher = obj.teacher_fn+' '+obj.teacher_ln;
     this.imgSrc=url+'public/users_files/images/groups_photo/'+obj.photo;
+    this.groupId=obj.group_id;
 }
 var viewModel = new ViewModel();
 viewModel.activate();
 ko.applyBindings(viewModel);
+
+//test ajax
+
+
+
+var test=function(){
+    $.ajax({
+        url: url+'app/groups/moveToArchive/1/0',
+        type: 'GET',
+        success: function(response){
+            console.log(response);
+        },
+        error: function (xhr){
+            alert("Error! "+xhr);
+        }
+    });
+}
