427ccd4a197bea67a6829dc36487d3075b7f5989
Fylhtq14 <vfreiby@gmail.com>
2015-02-13 11:32:37 +0200

    доробив архівацію груп

--- src/public/js/app/groups/groups.js
+++ src/public/js/app/groups/groups.js
@@ -2,7 +2,23 @@ function ViewModel(){
     var that = this;
     that.groups = ko.observableArray([]);
     that.currentId="";
-    that.startEdit=function(name){
+    that.archivate=function(obj){
+        $.ajax({
+            url: url+'app/groups/moveToArchive/'+obj.groupId+'/'+1,
+            type: 'GET',
+            success: function(){
+               for(var i =0;i<that.groups().length;i++){
+                  if(that.groups()[i].groupId==obj.groupId){
+                      that.groups()[i].archived(true);
+                  }
+               }
+            },
+            error: function (xhr){
+                alert("Error! "+xhr);
+            }
+        });
+    };
+      that.startEdit=function(name){
         for(var i=0; i<that.groups().length; i++){
             if (that.groups()[i].name==name){
                 that.groups()[i].edit(true);
@@ -17,11 +33,12 @@ function ViewModel(){
                 var group = new Group(response[i]);
                 group.host = ko.observable(that.currentId==response[i].teacher_id);
                 group.name = ko.observable(response[i].name);
-                group.description = ko.observable(response[i].description);
+                group.description = ko.observable(response[i].descr);
                 group.archived = ko.observable(response[i].archived==1);
                 group.edit = ko.observable();
                 that.groups.push(group);
             }
+
             that.groups.sort(function(left, right) { return left.name == right.name ? 0 : (left.name < right.name ? -1 : 1) });
         })
     }
@@ -35,6 +52,8 @@ function Group(obj){
     this.goAway = function(){
         window.location = this.groupLink;
     }
+    this.groupId=obj.group_id;
+
 }
 var viewModel = new ViewModel();
 viewModel.activate();
