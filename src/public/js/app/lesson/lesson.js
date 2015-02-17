a14aba032e02584ba17b825fc745fbde9a4267b4
AndreyUser <vfreiby@gmail.com>
2015-02-18 01:25:24 +0200

    1) поправил верстку 2) удаление ссылок

--- src/public/js/app/lesson/lesson.js
+++ src/public/js/app/lesson/lesson.js
@@ -33,28 +33,16 @@ function ViewModel()
           that.makeArray()
       }
     };
-    that.loadFile=function(formElement){
+    that.deleteLink=function(link){
+        for(var i =0;i<that.links().length;i++){
 
-
-        $.ajax({
-            url: url+'app/lesson/upload/',
-            type: 'POST',
-            processData:false,
-            contentType:false,
-            data: new FormData(formElement),
-            success: function(response){
-
-                response.url= url+'public/users_files/tasks/'+response.newName ;
-                that.files.push(response);
-                that.makeArray();
-
-            },
-            error: function(xhr){
-                fail(xhr);
+            if(that.links()[i].name==link){
+                that.links.remove(that.links()[i])
             }
-        });
+        }
+        that.makeArray();
     };
-     that.makeArray=function(){
+       that.makeArray=function(){
         var data={
             description: that.homeWorkDescription(),
             links: that.links(),
@@ -79,7 +67,30 @@ function ViewModel()
         }
         sendData()
     };
-
+    ko.bindingHandlers.loadFile={
+        init:function(element, valueAccessor, allBindings,currentContext,  viewModel) {
+            $(element).change(function(){
+                if(element.firstChild.nextElementSibling.files[0].size<20971520) {
+                    $.ajax({
+                        url: url + 'app/lesson/upload/',
+                        type: 'POST',
+                        processData: false,
+                        contentType: false,
+                        data: new FormData(element),
+                        success: function (response) {
+                            response.url = url + 'public/users_files/tasks/' + response.newName;
+                            that.files.push(response);
+                            that.makeArray();
+                        },
+                        error: function (xhr) {
+                           alert('pp')
+                        }
+                    });
+                }
+else{alert('хуй вам')}
+            })
+        }
+    };
     //method that starts magic
     that.activate = function () {
         var lessonId = window.location.pathname;
