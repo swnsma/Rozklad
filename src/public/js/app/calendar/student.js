0b6c0cec90b0f603447ec959ba0aaeac11bb8bd3
Таня <taaania@bk.ru>
2015-02-18 01:26:39 +0200

    Студент бачить завдання в Якихи є Дедлайн, в окремому додатку

--- src/public/js/app/calendar/student.js
+++ src/public/js/app/calendar/student.js
@@ -2,6 +2,65 @@
  * Created by Таня on 23.01.2015.
  */
 
+function createListLeson(data,parent){
+    for(var i =0;i<data.length;++i){
+        if(data[i].deadline){
+            var $div = $('<div>');
+            $div.appendTo(parent);
+            $div.attr({
+                'id':'lesson'+data[i].id
+            })
+            $div.on('click',function(){
+                debugger;
+                window.location=url+'app/lesson/id'+$(this).attr('id').substr(6);
+            });
+            $div.addClass('lesson-wrapper');
+
+            var $eventTitle = $('<span>');
+            $eventTitle.appendTo($div);
+            $eventTitle.text(data[i].title);
+            $eventTitle.addClass('event-title');
+
+            for(var j =0;j<data[i].group.length;++j){
+                var $var=$('<span>');
+                $var.appendTo($div);
+                $var.text(data[i].group[j].name[0]);
+                $var.css({
+                    'display': 'inline-block',
+                    'width': '10px',
+                    'height': '10px',
+                    'fontSize': '8px',
+                    'textAlign': 'center',
+                    'marginLeft': '2px',
+                    'marginRight':'2px',
+                    'borderRadius': '2px',
+                    'verticalAlign': 'baseline',
+                    'lineHeight':'10px',
+                    'backgroundColor': data[i].group[j].color,
+                    'fontWeight': 'normal',
+                    'verticalAlign':'middle',
+                    'color':'white'
+                });
+            }
+            if (data[i].teacher) {
+                var $var = $('<span>');
+                $var.text(data[i].name[0] + '.' + data[i].surname);
+                $var.css({
+                    'fontSize': '10px',
+                    'display': 'inline-block',
+                    'lineHeight':'10px'
+                });
+                $var.appendTo($div);
+            }
+
+            var $var = $('<span>');
+            $var.text(data[i].deadline);
+            $var.addClass('deadline');
+            $var.appendTo($div);
+
+        }
+    }
+}
 
 function Calendar_student(){
     var event=[];
@@ -29,74 +88,8 @@ function Calendar_student(){
                     function(data){
                         event= data;
                         callback(data);
-                        for(var i =0;i<data.length;++i){
-                            if(data[i].deadline){
-                                debugger;
-                                var $div = $('<div>');
-                                $div.appendTo(self.jqueryObject.deadlineTask.deadlineTaskContentContent);
+                        createListLeson(data,self.jqueryObject.deadlineTask.deadlineTaskContentContent);
 
-                                $div.css({
-                                    'borderBottom':'1px solid #aaa',
-                                    'padding':'1px 10px'
-                                });
-
-                                var $eventTitle = $('<span>');
-                                $eventTitle.appendTo($div);
-                                $eventTitle.text(data[i].title);
-                                $eventTitle.css({
-                                    'color':'#333',
-                                    'font':'12px arial,sans-serif',
-                                    'fontWeight':'600',
-                                    'display':'block',
-                                    'margin':'0 0 -5px',
-                                    'padding':0
-                                });
-
-                                for(var j =0;j<data[i].group.length;++j){
-                                    var $var=$('<span>');
-                                    $var.appendTo($div);
-                                    $var.text(data[i].group[j].name[0]);
-                                    $var.css({
-                                        'display': 'inline-block',
-                                        'width': '10px',
-                                        'height': '10px',
-                                        'fontSize': '8px',
-                                        'textAlign': 'center',
-                                        'marginLeft': '2px',
-                                        'marginRight':'2px',
-                                        'borderRadius': '2px',
-                                        'verticalAlign': 'baseline',
-                                        'lineHeight':'10px',
-                                        'backgroundColor': data[i].group[j].color,
-                                        'fontWeight': 'normal',
-                                        'verticalAlign':'middle',
-                                        'color':'white'
-                                    });
-                                }
-                                if (data[i].teacher) {
-                                    var $var = $('<span>');
-                                    $var.text(data[i].name[0] + '.' + data[i].surname);
-                                    $var.css({
-                                        'fontSize': '10px',
-                                        'display': 'inline-block',
-                                        'lineHeight':'10px'
-                                    });
-                                    $var.appendTo($div);
-                                }
-
-                                var $var = $('<span>');
-                                $var.text(data[i].deadline);
-                                $var.css({
-                                    'fontSize': '10px',
-                                    'display': 'block',
-                                    'fontWeight':'bold',
-                                    'color':'#aaa',
-                                    'lineHeight':'10px'
-                                });
-                                $var.appendTo($div);
-
-                            }
-                        }
                     },
                     function(er){
                         alert('Лалала');
