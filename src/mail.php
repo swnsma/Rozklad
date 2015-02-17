eb21e495cb83a608b0fe4a7baf9b36e67b2fce0e
konstanchuk <vova.konstanchuk@gmail.com>
2015-02-17 20:17:04 +0200

    підправив дизайн листа для вчителя

--- src/mail.php
+++ src/mail.php
@@ -7,29 +7,39 @@ require_once(__DIR__ . '/lib/mail/class.smtp.php');
 require_once(__DIR__ . '/conf/conf.php');
 require_once(__DIR__ . '/core/Mail.php');
 
-//date_default_timezone_set("Europe/Kiev");
-//$date = date('n/d/Y h:i', time());
+/*
+    Приклади
+*/
 
-$m = Mail::getInstance();
-/*$template = $m->getTemplate('letterToTeacher', array( "userName"=>"ИванБобров", "date"=>$date,
-    'link' => 'vk.com'
-));*/
-$template = $m->getTemplate('example', array(
-    'link' => 'vk.com',
-    'image' => 'image'
-));
+// повідлення для вчителя
+function example1() {
+    $m = Mail::getInstance();
 
-if (is_null($template)) {
-    echo 'template is not exists';
-} else {
-    //$m->addFile(DOC_ROOT . 'public/img/bg.jpg', 'images.jpg');
-    $m->addFileToHtml(DOC_ROOT . 'public/img/bg.jpg', 'image');
-    if ($m->send(array('swnsma@gmail.com'), 'subject', $template)) {
-        echo 'true';
+    $template = $m->getTemplate('letterToTeacher2', array(
+        'userName' => 'User Name',
+        'mail_background' => 'mail_background',
+        'mail_sep' => 'mail_sep',
+        'url' => 'http://google.com'
+    ));
+
+    if (is_null($template)) {
+        echo 'template is not exists';
     } else {
-        echo 'false';
-        echo $m->getErrorInfo();
-    }
+        //$m->addFile(DOC_ROOT . 'public/img/bg.jpg', 'images.jpg');
+        $m->addFileToHtml(DOC_ROOT . 'public/img/mail_background.png', 'mail_background');
+        $m->addFileToHtml(DOC_ROOT . 'public/img/mail_sep.png', 'mail_sep');
+        if ($m->send(array(
+                'myrozklad@mail.ru'
+            ), 'subject', $template)) {
+            echo 'true';
+        } else {
+            echo 'false';
+            echo $m->getErrorInfo();
+        }
 
+    }
 }
+
+example1();
+
 ?>
