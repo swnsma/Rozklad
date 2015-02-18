70c488051bc1cf36ba1caa277b64d90f0434d405
konstanchuk <vova.konstanchuk@gmail.com>
2015-02-18 15:24:28 +0200

    вибір почт студентів по урокам

--- src/core/MailerOfLetter.php
+++ src/core/MailerOfLetter.php
@@ -10,8 +10,7 @@ class MailerOfLetter {
         $this->db = DataBase::getInstance()->DB();
     }
 
-
-    private function getUsersForInvitationToLesson() {
+    private function getGroupsForInvitationToLesson() {
         $request = <<<HERE
             SELECT group_id, lesson_id FROM group_lesson WHERE mail = 0
 HERE;
@@ -24,10 +23,43 @@ HERE;
         return null;
     }
 
+    private function getEmailUsersGroups($id) {
+        $request = <<<HERE
+            SELECT
+                `user`.`email` as email
+            FROM `user`
+            WHERE `user`.`id` IN (
+                SELECT
+                    `student_group`.`student_id` as id
+                FROM `student_group`
+                WHERE `student_group`.`group_id` = :id
+                )
+HERE;
+        try {
+            $request = $this->db->prepare($request);
+            $request->bindParam(':id', $id, PDO::PARAM_INT);
+            if ($request->execute()) return $request->fetchColumn(0);
+        } catch(PDOException $e) {
+            print $e->getMessage();
+        }
+        return null;
+    }
+
     public function sendInvitationToLesson() {
+        $groups = $this->getGroupsForInvitationToLesson();
+        foreach($groups as $group) {
+            $emails = $this->getEmailUsersGroups($group['group_id']);
+
+            echo '<pre>';
+            print_r($emails);
+            echo '</pre>';
+        }
+
+        /*
+
         echo '<pre>';
-        print_r($this->getUsersForInvitationToLesson());
-        echo '</pre>';
+        print_r($emails);
+        echo '</pre>';*/
     }
 
     static public function getInstance() {
