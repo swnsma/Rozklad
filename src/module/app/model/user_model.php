087f6c75e100ad009ab57200d4b2a6dec86f5061
AndreyUser <vfreiby@gmail.com>
2015-02-19 18:55:50 +0200

    Загрузка стеднтом домашки

--- src/module/app/model/user_model.php
+++ src/module/app/model/user_model.php
@@ -1,29 +1,34 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Таня
  * Date: 25.01.2015
  * Time: 20:38
  */
-class UserModel extends Model {
-    public function __construct() {
+class UserModel extends Model
+{
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function getUserInfo($id){
-        try{
-        $d=$this->db->query("SELECT* FROM `user` WHERE id=$id;")->fetchAll(PDO::FETCH_ASSOC);
-        if(isset($d[0])){
-            return $d[0];
-        }
-        }
-        catch(PDOException $e){
+    public function getUserInfo($id)
+    {
+        try {
+            $d = $this->db->query("SELECT* FROM `user` WHERE id=$id;")->fetchAll(PDO::FETCH_ASSOC);
+            if (isset($d[0])) {
+                return $d[0];
+            }
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
-    public function getCurrentUserInfo(){
+
+    public function getCurrentUserInfo()
+    {
         $id = Session::get('id');
-        $userInfo=Array();
+        $userInfo = Array();
         $sql = <<<SQL
                 select
                     user.name,
@@ -41,75 +46,82 @@ class UserModel extends Model {
 SQL;
         $userInfo = $this->db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-        if(count($userInfo)===0){
+        if (count($userInfo) === 0) {
             return null;
         }
         return $userInfo[0];
     }
 
-    public function getInfoFB($fb_id){
+    public function getInfoFB($fb_id)
+    {
         try {
             $request = <<<TANIA
 select * from user
 where fb_id='$fb_id'
 TANIA;
-            $var =$this->db->query($request)->fetchAll(PDO::FETCH_ASSOC);
-            if(isset($var[0]))
+            $var = $this->db->query($request)->fetchAll(PDO::FETCH_ASSOC);
+            if (isset($var[0]))
                 return $var[0];
             else return null;
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return null;
         }
     }
-    public function getInfoGM($gm_id){
+
+    public function getInfoGM($gm_id)
+    {
         try {
             $request = <<<TANIA
             select * from user
             where gm_id='$gm_id'
 TANIA;
 
-            $var =$this->db->query($request)->fetchAll(PDO::FETCH_ASSOC);
+            $var = $this->db->query($request)->fetchAll(PDO::FETCH_ASSOC);
             return $var[0];
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return null;
         }
     }
-    public function getIdFB($fb_id){
+
+    public function getIdFB($fb_id)
+    {
         try {
             $request = <<<TANIA
 select id from user
 where fb_id='$fb_id'
 TANIA;
-            $var =$this->db->query($request)->fetchAll(PDO::FETCH_ASSOC);
-            if(isset($var[0]['id']))
-            {return $var[0]['id'];
-            }
-            else return null;
-        } catch(PDOException $e) {
+            $var = $this->db->query($request)->fetchAll(PDO::FETCH_ASSOC);
+            if (isset($var[0]['id'])) {
+                return $var[0]['id'];
+            } else return null;
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return null;
         }
     }
-    public function getIdGM($gm_id){
+
+    public function getIdGM($gm_id)
+    {
         try {
             $request = <<<TANIA
             select id from user
             where gm_id='$gm_id'
 TANIA;
 
-            $var =$this->db->query($request)->fetchAll(PDO::FETCH_ASSOC);
+            $var = $this->db->query($request)->fetchAll(PDO::FETCH_ASSOC);
             return $var[0]['id'];
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return null;
         }
     }
 
 
-    public function getOurTeacher(){
-        $sql ="Select u.name, u.id, u.surname,
+    public function getOurTeacher()
+    {
+        $sql = "Select u.name, u.id, u.surname,
 uu.id as uu
         from 'user' as u
         INNER JOIN role as r ON
@@ -119,33 +131,37 @@ uu.id as uu
         WHERE r.title='teacher'";
         try {
             $date = $this->db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-            $result=[];
-            for($var =0; $var<count($date);++$var){
-                if(!isset($date[$var]['uu'])){
-                    array_push($result,$date[$var]);
+            $result = [];
+            for ($var = 0; $var < count($date); ++$var) {
+                if (!isset($date[$var]['uu'])) {
+                    array_push($result, $date[$var]);
                 }
             }
 //            print_r($result);
             return $result;
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return null;
         }
 
     }
-    public function checkUnconfirmed($id){
+
+    public function checkUnconfirmed($id)
+    {
         try {
-            $sql=<<<sql
+            $sql = <<<sql
                 select * from unconfirmed_user where id='$id'
 sql;
             $date = $this->db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
             return count($date);
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return null;
         }
     }
-    public function getInfo($lessonId) {
+
+    public function getInfo($lessonId)
+    {
         $r = <<<HERE
         SELECT
             `lesson`.`lesson_info` as lesson_info
@@ -157,38 +173,56 @@ HERE;
         try {
             $request = $this->db->query($r)->fetchAll(PDO::FETCH_ASSOC);
             return $request;
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return null;
         }
     }
-    public function newInfo ($lessonId,$value){
-        try{
-            $STH=$this->db->prepare("UPDATE lesson SET lesson_info = :value WHERE id=:id");
-            $STH->execute(array('value'=>$value,'id'=>$lessonId));
-        }
-        catch(PDOException $e){
+
+    public function newInfo($lessonId, $value)
+    {
+        try {
+            $STH = $this->db->prepare("UPDATE lesson SET lesson_info = :value WHERE id=:id");
+            $STH->execute(array('value' => $value, 'id' => $lessonId));
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
 
 
     }
-    public function saveTask ($studentId,$name,$lessonId){
+
+    public function saveTask($studentId, $name, $lessonId)
+    {
         $r = <<<HERE
         INSERT INTO `result` (owner, link,lesson_id) VALUES ($studentId,'$name',$lessonId);
+HERE;
+        try {
+            $request = $this->db->query($r)->fetchAll(PDO::FETCH_ASSOC);
+            return $request;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function loadTasks($lessonId)
+    {
+        $r = <<<HERE
+SELECT
+            `result`.`link` as link,
+             `user`.`name` as name,
+             `user`.`surname` as surname
+from `user`, `result`
+        WHERE `result`.`owner`=`user`.`id` AND `result`.`lesson_id` = $lessonId
 
 HERE;
 
-
-        try{
+        try {
             $request = $this->db->query($r)->fetchAll(PDO::FETCH_ASSOC);
             return $request;
-        }
-        catch(PDOException $e){
+        } catch (PDOException $e) {
             echo $e->getMessage();
+            return null;
         }
-
-
     }
 
 }
