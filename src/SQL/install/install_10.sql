36191a4559cc76fb8ec6dd9da5fdcc55cd90a24f
swnsma <swnsma@gmail.com>
2015-02-19 11:50:07 +0200

    1)Oops

--- src/SQL/install/install_10.sql
+++ src/SQL/install/install_10.sql
@@ -1,2 +1,2 @@
 ALTER TABLE 'lesson' ADD COLUMN 'deadline'TEXT NULL;
-CREATE TABLE IF NOT EXISTS "result" ("id" integer NULL PRIMARY KEY AUTOINCREMENT,"link" text NULL,"grade" text NULL, "date" text NULL, "owner" integer NULL,"lesson_id" integer NULL);
+CREATE TABLE IF NOT EXISTS "result" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,"link" text NULL,"grade" text NULL, "date" text NULL, "owner" integer NULL,"lesson_id" integer NULL);
