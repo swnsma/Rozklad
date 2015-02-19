bf6d60d20e9f6b10b0186666d7dfdab5d5919096
Таня <taaania@bk.ru>
2015-02-19 11:43:03 +0200

    ijnj nfv

--- src/public/js/app/calendar/student.js
+++ src/public/js/app/calendar/student.js
@@ -3,6 +3,7 @@
  */
 
 function createListLeson(data,parent){
+    parent.empty();
     for(var i =0;i<data.length;++i){
         if(data[i].deadline){
             var $div = $('<div>');
@@ -11,7 +12,6 @@ function createListLeson(data,parent){
                 'id':'lesson'+data[i].id
             })
             $div.on('click',function(){
-                debugger;
                 window.location=url+'app/lesson/id'+$(this).attr('id').substr(6);
             });
             $div.addClass('lesson-wrapper');
@@ -54,12 +54,48 @@ function createListLeson(data,parent){
             }
 
             var $var = $('<span>');
-            $var.text(data[i].deadline);
+            var deadline = data[i].deadline;
+            $var.text(deadline.substr(0,deadline.length-3));
             $var.addClass('deadline');
             $var.appendTo($div);
 
+
+            var $timeTrack = $('<span>');
+            var currentData = new Date();
+            deadline = new Date(deadline);
+
+            deadline.setFullYear(deadline.getFullYear()-currentData.getFullYear());
+            deadline.setMonth(deadline.getMonth()-currentData.getMonth());
+            deadline.setDate(deadline.getDate()-currentData.getDate());
+            deadline.setHours(deadline.getHours()-currentData.getHours());
+            deadline.setMinutes(deadline.getMinutes()-currentData.getMinutes());
+
+            debugger;
+            deadline = normDate(deadline.getFullYear(),toFormat(deadline.getMonth()+1),deadline.getDate(),deadline.getHours(),deadline.getMinutes());
+
+            $timeTrack.text(deadline.substr(0,deadline.length-3));
+            $timeTrack.attr({
+                'id':'timeTrack'+i
+            });
+
+
+
+            $timeTrack.addClass('timeTrack');
+            $timeTrack.appendTo($div);
+
         }
     }
+
+    setInterval(function(){
+        for(var i =0;i<data.length;++i) {
+            deadline = $('#timeTrack'+i).text();
+            console.log(deadline);
+            deadline = new Date(deadline);
+            deadline.setMinutes(deadline.getMinutes() - 1);
+            deadline = normDate(deadline.getFullYear(), toFormat(deadline.getMonth() + 1), deadline.getDate(), deadline.getHours(), deadline.getMinutes());
+            $('#timeTrack'+i).text(deadline.substr(0, deadline.length - 3));
+        }
+    },1000);
 }
 
 function Calendar_student(){
@@ -67,18 +103,23 @@ function Calendar_student(){
     Calendar.call(this);
     var self = this;
 
+    var windowEvent=false;
+
     this.jqueryObject.deadlineTask ={
         deadlineTaskBt:$('#deadlineTaskBt'),
         deadlineTaskContent:$('#deadlineTaskContent'),
         deadlineTaskContentTitle:$('#deadlineTaskContentTitle'),
         deadlineTaskClose: $('#deadlineTaskClose'),
-        deadlineTaskContentContent:$('#deadlineTaskContentContent')
+        deadlineTaskContentContent:$('#deadlineTaskContentContent'),
+        deadlineTaskResize: $('#deadlineTaskResize')
     }
     this.option.eventSources= [
         {
             events: function(start, end, timezone, callback) {
                 start=start._d;
                 end=end._d;
+                end =  new Date(end);
+                end.setMonth(end.getMonth()+1);
                 var start1 = normDate(start.getFullYear(),start.getMonth()+1,start.getDay(),start.getHours(),start.getMinutes());
                 var end1 = normDate(end.getFullYear(),end.getMonth()+1,end.getDay(),end.getHours(),end.getMinutes());
 
@@ -108,37 +149,108 @@ function Calendar_student(){
 
     this.jqueryObject.deadlineTask.deadlineTaskBt.on('click',function(){
         self.jqueryObject.deadlineTask.deadlineTaskBt.hide();
+
+        if(!windowEvent){
+            var deadlineTaskContent = self.jqueryObject.deadlineTask.deadlineTaskContent;
+            self.jqueryObject.deadlineTask.deadlineTaskContent.css({
+                'left':document.documentElement.clientWidth-deadlineTaskContent.css('width').substr(0,deadlineTaskContent.css('width').length-2)-30,
+                'top':document.documentElement.clientHeight-deadlineTaskContent.css('height').substr(0,deadlineTaskContent.css('height').length-2)
+
+            });
+        }
+        windowEvent = true;
         self.jqueryObject.deadlineTask.deadlineTaskContent.show();
     });
+
     this.jqueryObject.deadlineTask.deadlineTaskClose.on('click',function(){
         self.jqueryObject.deadlineTask.deadlineTaskBt.show();
         self.jqueryObject.deadlineTask.deadlineTaskContent.hide();
     });
-    var drag;
-    var clientX;
-    var clientY;
-    this.jqueryObject.deadlineTask.deadlineTaskContentTitle.on('mousedown',function(e){
-        if(e.button===0)
-        drag=self.jqueryObject.deadlineTask.deadlineTaskContent;
-        clientX= e.clientX;
-        clientY= e.clientY;
-    });
-    this.jqueryObject.deadlineTask.deadlineTaskContentTitle.on('mouseup',function(){
-        drag=null;
-    });
-    $(document).on('mousemove',function(e){
-        if(drag) {
-            drag.css({
-                'right':+drag.css('right').substr(0, drag.css('right').length - 2)-(e.clientX-clientX),
-                'bottom':+drag.css('bottom').substr(0, drag.css('bottom').length - 2)-(e.clientY-clientY)
-            });
-        }
-        clientX= e.clientX;
-        clientY= e.clientY;
-    });
 
+    this.dragEvent = function() {
+        var drag;
+        var clientX;
+        var clientY;
+        var bool=false;
+        this.jqueryObject.deadlineTask.deadlineTaskContentTitle.on('mousedown', function (e) {
+            if (e.button === 0)
+                drag = self.jqueryObject.deadlineTask.deadlineTaskContent;
+            clientX = e.clientX;
+            clientY = e.clientY;
+        });
+
+
+        this.jqueryObject.deadlineTask.deadlineTaskContentTitle.on('mouseup', function () {
+            drag = null;
+        });
+        $(document).on('mousemove', function (e) {
+            if (drag) {
+                drag.css({
+                    'left': +drag.css('left').substr(0, drag.css('left').length - 2) + (e.clientX - clientX),
+                    'top': +drag.css('top').substr(0, drag.css('top').length - 2) + (e.clientY - clientY)
+                });
+            }
+            clientX = e.clientX;
+            clientY = e.clientY;
+        });
+
+    }
 
     //$(".nano").nanoScroller();
+
+    this.resizeDeadline = function(){
+        var resize;
+        var clientX;
+        var clientY;
+        var realHeight;
+        var realWight;
+        this.jqueryObject.deadlineTask.deadlineTaskResize.on('mousedown',function(e) {
+            if (e.button === 0) {
+                resize = self.jqueryObject.deadlineTask.deadlineTaskContent;
+                clientX = e.clientX;
+                clientY = e.clientY;
+                debugger;
+                realWight = +resize.css('width').substr(0, resize.css('width').length - 2);
+                realHeight = +resize.css('height').substr(0, resize.css('height').length - 2);
+
+            }
+        });
+        $(document).on('mouseup', function () {
+            resize = null;
+        });
+        $(document).on('mousemove', function (e) {
+            if (resize) {
+                var wight = +resize.css('width').substr(0, resize.css('width').length - 2) + (e.clientX - clientX);
+                realWight = realWight+ (e.clientX - clientX);
+                realHeight = realHeight +(e.clientY - clientY);
+                if(realWight<=230){
+                    wight = 230;
+                }else {
+                    wight = realWight;
+                }
+                var height = +resize.css('height').substr(0, resize.css('height').length - 2) + (e.clientY - clientY);
+                if(realHeight<=70){
+                    height = 70;
+                }else{
+                    height=realHeight;
+                }
+                resize.css({
+                    'width': wight,
+                    'height': height
+                });
+
+                var cont = self.jqueryObject.deadlineTask.deadlineTaskContentContent;
+
+                cont.css({
+                    'width': +wight,
+                    'height': height-30
+                });
+            }
+            clientX = e.clientX;
+            clientY = e.clientY;
+        });
+
+    }
 
     this.jqueryObject.calendar.fullCalendar(this.option);
 
@@ -148,6 +260,8 @@ $(document).ready(function() {
     var calendar = new Calendar_student();
     calendar.getCurrentUser();
     calendar.realTimeUpdate();
+    calendar.dragEvent();
+    calendar.resizeDeadline();
 
 });
 
