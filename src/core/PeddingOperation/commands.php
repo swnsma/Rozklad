d83d01d4d5232989a631f31a5f49968d3e85cf66
konstanchuk <vova.konstanchuk@gmail.com>
2015-02-20 10:40:01 +0200

    "фон" на сторінці груп

--- src/core/PeddingOperation/commands.php
+++ src/core/PeddingOperation/commands.php
@@ -1,8 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 require_once __DIR__ . '/../../conf/conf.php';
 require_once DOC_ROOT . 'core/DataBase.php';
 require_once DOC_ROOT . '/core/MailerOfLetter.php';
