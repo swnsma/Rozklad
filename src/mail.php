8c6334bc16ff104a6c96d6908ac0a2b7ba006c7c
konstanchuk <vova.konstanchuk@gmail.com>
2015-02-24 13:13:51 +0200

    відправка почти з яндекса виправлена

--- src/mail.php
+++ src/mail.php
@@ -1,79 +1,12 @@
 <?php
 
-error_reporting(E_ALL);
+require_once __DIR__ . '/lib/mail/class.phpmailer.php';
+require_once __DIR__ . '/lib/mail/class.smtp.php';
+require_once __DIR__ . '/conf/conf.php';
+require_once __DIR__ . '/core/Mail.php';
+require_once __DIR__ . '/core/DataBase.php';
+require_once __DIR__ . '/core/PeddingOperation/PeddingOperation.php';
 
-require_once(__DIR__ . '/lib/mail/class.phpmailer.php');
-require_once(__DIR__ . '/lib/mail/class.smtp.php');
-require_once(__DIR__ . '/conf/conf.php');
-require_once(__DIR__ . '/core/Mail.php');
-require_once(__DIR__ . '/core/DataBase.php');
-
-/*
-    Приклади
-*/
-
-// повідлення для вчителя
-function example1() {
-    $m = Mail::getInstance();
-    $template = $m->getTemplate('letterToTeacher2', array(
-        'userName' => 'User Name',
-        'mail_background' => 'mail_background',
-        'mail_sep' => 'mail_sep',
-        'url' => 'http://google.com'
-    ));
-    if (is_null($template)) {
-        echo 'template is not exists';
-    } else {
-        //$m->addFile(DOC_ROOT . 'public/img/bg.jpg', 'images.jpg');
-        $m->addFileToHtml(DOC_ROOT . 'public/img/mail_background.png', 'mail_background');
-        $m->addFileToHtml(DOC_ROOT . 'public/img/mail_sep.png', 'mail_sep');
-        if ($m->send(array(
-            'myrozklad@mail.ru'
-        ), 'subject', $template)) {
-            echo 'true';
-        } else {
-            echo 'false';
-            echo $m->getErrorInfo();
-        }
-    }
-}
-
-// повідомлення для адміна
-function example2() {
-    $m = Mail::getInstance();
-    $template = $m->getTemplate('letterToAdmin2', array(
-        'userName' => 'User Name',
-        'phone' => '4324323',
-        'email' => 'sdfef34fsfef3@gmail.com',
-        'mail_background' => 'mail_background',
-        'url' => 'http://google.com'
-    ));
-    if (is_null($template)) {
-        echo 'template is not exists';
-    } else {
-        $m->addFileToHtml(DOC_ROOT . 'public/img/mail_background2.jpg', 'mail_background');
-        if ($m->send(array(
-            'myrozklad@mail.ru'
-        ), 'subject', $template)) {
-            echo 'true';
-        } else {
-            echo 'false';
-            echo $m->getErrorInfo();
-        }
-    }
-}
-
-require_once 'core/PeddingOperation/PeddingOperation.php';
-//require_once 'module/app/model/lesson_model.php';
-
-if (DEBUG) {
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-} else {
-    error_reporting(0);
-    ini_set('display_errors', 0);
-}
 PeddingOperation::run();
-
 
 ?>
