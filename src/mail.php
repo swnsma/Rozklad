95b522b5a2ccb95f20865c9a3c4e2fbe47ace98e
konstanchuk <vova.konstanchuk@gmail.com>
2015-02-24 13:10:22 +0200

    скачування файлів

--- src/mail.php
+++ src/mail.php
@@ -1,13 +1,11 @@
 <?php
 
-error_reporting(E_ALL);
-
-require_once(__DIR__ . '/lib/mail/class.phpmailer.php');
-require_once(__DIR__ . '/lib/mail/class.smtp.php');
-require_once(__DIR__ . '/conf/conf.php');
-require_once(__DIR__ . '/core/Mail.php');
-require_once(__DIR__ . '/core/DataBase.php');
-require_once 'core/PeddingOperation/PeddingOperation.php';
+require_once __DIR__ . '/lib/mail/class.phpmailer.php';
+require_once __DIR__ . '/lib/mail/class.smtp.php';
+require_once __DIR__ . '/conf/conf.php';
+require_once __DIR__ . '/core/Mail.php';
+require_once __DIR__ . '/core/DataBase.php';
+require_once __DIR__ . '/core/PeddingOperation/PeddingOperation.php';
 
 PeddingOperation::run();
 
