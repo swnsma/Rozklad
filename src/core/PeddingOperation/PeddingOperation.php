4eafddd2e9a1c694456a91b514867a46756bd2f1
konstanchuk <vova.konstanchuk@gmail.com>
2015-02-24 12:16:06 +0200

    скачування файлів

--- src/core/PeddingOperation/PeddingOperation.php
+++ src/core/PeddingOperation/PeddingOperation.php
@@ -2,18 +2,15 @@
 
 class PeddingOperation {
     public static function run() {
-        $fp = fsockopen($_SERVER['SERVER_NAME'], $_SERVER['SERVER_PORT'], $errno, $errstr, 30);
+        $fp = fsockopen("localhost", 83, $errno, $errstr, 30);
         if (!$fp) {
             echo "$errstr ($errno)<br />\n";
         } else {
-            $out = "POST /core/PeddingOperation/commands.php HTTP/1.0\r\n";
-            $out .= "Host: rozklad.z-tech.com.ua\r\n";
+            $out = "POST /src/PeddingOperation/commands.php HTTP/1.0\r\n";
+            $out .= "Host: localhost:83\r\n";
             $out .= "Content-Type: application/x-www-form-urlencoded\r\n";
             $out .= "Connection: Close\r\n\r\n";
             fwrite($fp, $out);
-            /*while (!feof($fp)) {
-                echo fgets($fp, 128);
-            }*/
             fclose($fp);
         }
     }
