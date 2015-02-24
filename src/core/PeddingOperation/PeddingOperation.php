d7d4e1a2b9f87bbc3282a8d4e3a0163653bde8ee
test <test@test.com>
2015-02-24 12:55:30 +0200

    ssd

--- src/core/PeddingOperation/PeddingOperation.php
+++ src/core/PeddingOperation/PeddingOperation.php
@@ -6,15 +6,15 @@ class PeddingOperation {
         if (!$fp) {
             echo "$errstr ($errno)<br />\n";
         } else {
-            $out = "POST /core/PeddingOperation/commands.php HTTP/1.0\r\n";
-            $out .= "Host: rozklad.z-tech.com.ua\r\n";
-            $out .= "Content-Type: application/x-www-form-urlencoded\r\n";
+            $out = "GET http://rozklad.z-tech.com.ua/mail.php HTTP/1.0\r\n";
             $out .= "Connection: Close\r\n\r\n";
             fwrite($fp, $out);
-            /*while (!feof($fp)) {
+            while (!feof($fp)) {
                 echo fgets($fp, 128);
-            }*/
+            }
             fclose($fp);
         }
     }
 }
+
+?>
