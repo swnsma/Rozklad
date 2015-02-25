e791fb203478dabb2dbecb0da2338a84e590c9c1
swnsma <swnsma@gmail.com>
2015-02-25 10:51:38 +0200

    1)Oops

--- src/SQL/install/install_12.sql
+++ src/SQL/install/install_12.sql
@@ -1,1 +1,1 @@
-CREATE TABLE IF NOT EXISTS "result" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,"link" text NOT NULL, "grade" text NOT NULL,"date" text NOT NULL,"owner" integer NOT NULL, "lesson_id" integer NOT NULL);
+CREATE TABLE IF NOT EXISTS "result" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,"link" text  NULL, "grade" text  NULL,"date" text  NULL,"owner" integer  NULL, "lesson_id" integer  NULL);
