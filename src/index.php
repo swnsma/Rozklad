9f8a5444863dd8d96cdd60eb91f45f92cdc06660
kovalenko0 <koolse@outlook.com>
2015-02-26 10:57:43 +0200

    disable some modules

--- src/index.php
+++ src/index.php
@@ -23,10 +23,10 @@ if (DEBUG) {
     ini_set('display_errors', 0);
 }
 Base_Install::Run();
-LessonModel::realDeletedLesson();
+//LessonModel::realDeletedLesson();
 //Base_Install::LoadDummy();
 
 $app = new Bootstrap();
-PeddingOperation::run();
+//PeddingOperation::run();
 
 ?>
