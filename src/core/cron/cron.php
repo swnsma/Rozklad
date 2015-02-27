6829b7a597625b2df205ae3b177e55642eb8535e
konstanchuk <vova.konstanchuk@gmail.com>
2015-02-27 13:04:05 +0200

    шляхи

--- src/core/cron/cron.php
+++ src/core/cron/cron.php
@@ -1,9 +1,8 @@
 <?php
 
-set_include_path('/var/www/vhosts/rozklad_test/public_html/');
+set_include_path('./../../');
 
 require_once 'conf/mail_conf.php';
-require_once 'core/cron/database.php';
 require_once 'core/cron/MailerOfLetter.php';
 
 $m = MailerOfLetter::getInstance();
