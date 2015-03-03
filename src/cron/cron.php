8412d30dff5cde996cf448b8d65cde3a8058174b
konstanchuk <vova.konstanchuk@gmail.com>
2015-03-03 23:09:21 +0200

    core/Model.php

--- src/cron/cron.php
+++ src/cron/cron.php
@@ -4,6 +4,7 @@ set_include_path('./../');
 
 require_once 'conf/mail_conf.php';
 require_once 'cron/MailerOfLetter.php';
+require_once 'core/Model.php';
 require_once 'module/app/model/lesson_model.php';
 
 $m = MailerOfLetter::getInstance();
